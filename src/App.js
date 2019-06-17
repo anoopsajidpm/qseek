a3ee90c7f8aed67f6d368b0c34a36af61fadb485
Anoop Sajid <pc06857@princesscruises.com>
2019-06-17 12:09:39 +0530

    minor changes in UI

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 
 import React from 'react';
-//import logo from './logo.svg';
 import './App.scss';
 import 'typeface-roboto';
+import Listview from './components/Listview/Listview';
+import Loader from './components/Loader/Loader';
+import Langs from './assets/json/langs.json';
+import Surahs from './assets/json/SurahList.json';
+import Popup from "reactjs-popup";
+import SurahInfo from './modules/SurahInfo/SurahInfo';
+
 //import TextField from '@material-ui/core/TextField';
 
 //import PropTypes from 'prop-types';
@@ -24,13 +30,6 @@ import 'typeface-roboto';
 //import CheckBoxIcon from '@material-ui/icons/CheckBox';
 //import Favorite from '@material-ui/icons/Favorite';
 //import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
-
-import Listview from './components/Listview/Listview';
-import Loader from './components/Loader/Loader';
-import Langs from './assets/json/langs.json';
-import Surahs from './assets/json/SurahList.json';
-import Popup from "reactjs-popup";
-import SurahInfo from './modules/SurahInfo/SurahInfo';
 
 //import Checkbox from './components/Checkbox/Checkbox';
 
@@ -84,7 +83,6 @@ class App extends React.Component {
       window.addEventListener('load', this.handleLoad);
     }
     
-    
     handleLoad(){
       //console.log(Langs);
       this.setState({
@@ -95,8 +93,6 @@ class App extends React.Component {
           surahList: Surahs.data,
           preloader: false
         });
-
-     
     }
 
     langPopupOnOpen(){
@@ -104,11 +100,7 @@ class App extends React.Component {
         langIsOpen: true
       })
     }
-
     validateInput(value){
-      //console.log(value);
-      //let selSurah = this.state.selectedSurah;
-      //let errMsg = '';
       let isValid = true;
       let totAyahs;
       console.log(value);
@@ -125,10 +117,7 @@ class App extends React.Component {
       console.log(value);
       return isValid;
     }
-
     searchForAyah(event) {
-     
-      //console.log(this.state.preloader);
       let filter = Number(this.state.inputVal);
       let q_editions = (this.state.q_edition_ar + ',' + this.state.q_edition_trans + ',' + this.state.q_edition_audio) ;
       this.setState({
@@ -150,9 +139,6 @@ class App extends React.Component {
           filter = this.state.selSurahNumber + ":" + filter;
         } 
       } 
-
-      
-     
       const cachedFetch = (url, options) => {
         // Use the URL as the cache key to sessionStorage
       let cacheKey = url
@@ -207,48 +193,11 @@ class App extends React.Component {
       );
     }
     
-    /*getErrMessage(src) {
-      let message = '';
-      //let inp = this.state.inputVal;
-      let splitV = this.state.inputVal.split(':');
-      let curSurah = this.state.selectedSurah;
-      if(src === 'list'){
-        message = 'Surah list data error';
-      } else {
-        if(splitV.length > 1){
-          if(Number(splitV[0]) > 114 || Number(splitV[0]) < 1){
-            message = 'Invalid Surah reference';
-          } else {
-            if(curSurah && (Number(splitV[1]) < 1 || Number(splitV[1]) > Number(curSurah.numberOfAyahs))){
-              message = 'Invalid Ayah number. Enter Ayah 1 through ' + curSurah.numberOfAyahs;
-            } else {
-              message = ''
-            }
-          }
-        } else {
-          if(Number(splitV[0]) < 1 || Number(splitV[0]) > 60232){
-            message = 'Invalid Ayah number. Enter any from 1 to 60232';
-          } else {
-            message = '';
-          }
-        }
-      }
-      return message;
-      
-    }*/
-    
      processData(results) {
       let res = [];
       let audio = '';
       let details = null;
       let result = this.state.rawData;
-
-      /*this.setState({ 
-        ayahDetails: {},
-        mainResult: [],
-        preloader: true
-      });*/
-      
       if(!result || !result.data){
         console.log('no data');
         return false;
@@ -310,10 +259,7 @@ class App extends React.Component {
           mainResult: res, 
           preloader: false
         });
-       
-        
         return true;
-      
     }
     
     updateInputVal(evt) {
@@ -321,7 +267,6 @@ class App extends React.Component {
       let isValid = this.validateInput(value);
       let navBackClass = this.state.navBtnClass.back;
       let navNextClass = this.state.navBtnClass.next; 
-      //console.log(evt.target.value);
       if(isValid){
         this.setState({
           inputVal: evt.target.value
@@ -347,7 +292,7 @@ class App extends React.Component {
 
     }
     verifyInputKey(evt) {
-      if(evt.keyCode === 186) { // || evt.keyCode === 189){
+      if(evt.keyCode === 186) { 
         this.setState({
           selSurahNumber: evt.target.value,
           inputVal: '', 
@@ -374,7 +319,6 @@ class App extends React.Component {
         }, () => {console.log(this.state.selectedSurah)});
       }
       
-      
       this.setState({ 
         ayahDetails: {},
         mainResult: [], 
@@ -382,52 +326,16 @@ class App extends React.Component {
       }, () => {this.ayahInput.focus()});
       
     }
-    
-    selectLang = evt => {
-      console.log(evt.target.getAttribute('data-value'));
-      
-      let lng = evt.target.getAttribute('data-value');
-      let eng = this.state.chkTrans.english;
-      let mal = this.state.chkTrans.malayalam;
-      switch (lng){
-        case 'en':
-          eng = true; //!eng;
-          mal = false;
-          break;
-        case 'ml':
-          mal = true; //!mal;
-          eng = false;
-          break;
-        default:
-      }
-      this.setState({
-        chkTrans:{
-          english: eng,
-          malayalam: mal
-        },
-        selectedTrans: this.findLang(Langs, lng),
-        langIsOpen: false
-      }, () => {
-          this.processData(this.state.rawData);
-      });
-      
-    } 
-    
-    
-
 findLang(array, title) {
-//console.log('find');
-  
-      let result = array.filter(item => item.code === title );
-      if(result.length){
-        return result[0];
-      } else {
-        return false;
-      }
+    let result = array.filter(item => item.code === title );
+    if(result.length){
+      return result[0];
+    } else {
+      return false;
+    }
 }
 
 navigateAyah = (evt) => {
-  //console.log(evt.target.getAttribute('data-value'));
   let data = evt.target.getAttribute('data-value');
   let valNow = Number(this.state.inputVal);
   let totAyahs = Number(this.state.selectedSurah.numberOfAyahs); 
@@ -436,13 +344,11 @@ navigateAyah = (evt) => {
   switch (data){
     case "back":  
       if(valNow > 1) {
-       // navBackClass = 'nav-btn back'; 
         valNow--;
       }
       break;
     case "next":
       if(valNow < totAyahs) {
-        //navNextClass = 'nav-btn next';
         valNow++;
       }
       break;
@@ -467,19 +373,8 @@ navigateAyah = (evt) => {
   
 }
   render(){
-    
-    /* <select id="surah-list" onChange={evt => this.changeSurah(evt)} className="surah-select">
-      <option value="0" >--Select--</option>
-      { 
-        surahs.map(el => <option value={el.number} key={el.number} > {el.number} - {el.englishName} </option>)
-      }
-    </select>
-    
-    <button className="toggle-btn" value="malayalam" onClick={this.chkSelectChange}>Mal</button>
-    */
     let listview;
 
-    //console.log(this.langs);
     if(this.state.mainResult.length){
       listview = <Listview 
         key={this.state.ayahDetails && this.state.number} 
@@ -487,55 +382,8 @@ navigateAyah = (evt) => {
         details={this.state.ayahDetails}
         />
     }
-    //console.log(this.findLang(Langs, 'ml'));
-    //let surah = {};
     const surahs = this.state.surahList;
-    //const filteredLangs = Langs.filter(item => item.code === 'en' || item.code === 'ml' );
-    //const chapters = Surahs.data;
-    //console.log(chapters);
-    //console.log(langs);
-   /*let selectedTrans = () => {
-    let selection = ''
-    if(this.state.chkTrans.english){
-      selection = 'English';
-    } else {
-      if(this.state.chkTrans.malayalam){
-        selection = 'Malayalam';
-      } else {
-        selection = 'None';
-      }
-    }
-    this.setState({
-      selectedTrans: selection
-    })
-    return selection;
-   }*/
     
-    /*const LangPopup =  () => (
-      <Popup
-        trigger={<a href="javascript:;" className="toggle-btn align-self-center margin top" value="Translation" >Translation: {selectedTrans} </a>}
-        on="click"
-        position="center center"
-        modal
-        closeOnDocumentClick
-      >
-      <div className="surah-wrapper">
-        <h3>Select Language:</h3>
-        <ul>
-        {
-          filteredLangs.map(lng=> 
-            <li 
-            data-value={lng.code} 
-            key={lng.code} 
-            onClick={this.selectLang} >{lng.code} - {lng.name} </li>
-          )
-        }
-        </ul>
-      </div>
-      </Popup>
-    )*/
-
-        
     const SurahPopup =  () => (
       <Popup
         trigger={<a href="javascript:;" value="Surahs" className="surah-button">Surah</a>}
@@ -568,36 +416,13 @@ navigateAyah = (evt) => {
         </ul>
       </div>
       </Popup>
-    
     )
-          
-    /*<label htmlFor="surah-list">Surah:</label>
-      <AppBar position="static" color="default">
-      <Toolbar>
-        <Typography variant="h1" size="small" color="inherit">
-          Q-Search
-        </Typography>
-      </Toolbar>
-    </AppBar>
-
-    <section className="titles-wrapper">
-        
-          <h2 className="surah-title">{selectedSurah.englishName} | {selectedSurah.name}</h2>
-          <p>Surah: <span className="clearer">{selectedSurah.number}</span> | {selectedSurah.englishNameTranslation}</p>
-          <LangPopup />
-          <div className="row-flex ayah-nav-wrapper margin top">
-            <a href="javascript:;" className={this.state.navBtnClass.back} data-value="back" onClick={this.navigateAyah}>Back</a>
-              <p>Ayah: <span className="clearer">{this.state.inputVal ? this.state.inputVal : 'Not Selected'}</span></p>
-            <a href="javascript:;" className={this.state.navBtnClass.next} data-value="next" onClick={evt => this.navigateAyah(evt)}>Next</a>
-           </div>
-      </section>
-    */
     const selectedSurah = this.state.selectedSurah;
    //alert(this.state.selectedTrans);
    const queryString = require('query-string');
 //var location = this.location;
 console.log(queryString);
-//console.log(queryString.parse(location.search));
+//console.log(queryString.parse(location.search)); <div className="triangle-right"><div className="inner"></div></div>
   return (
     <div className="page-wrapper">
       <header className="App-header">
@@ -616,7 +441,6 @@ console.log(queryString);
           navBackClass = {this.state.navBtnClass.back}
           navNextClass = {this.state.navBtnClass.next}
           langPopupOpen = {this.state.langIsOpen}
-          onSelectLang = {this.selectLang}
           selectedLang = {this.state.selectedTrans}
           translations = {this.state.chkTrans}
           inputVal = {this.state.inputVal}
@@ -624,14 +448,11 @@ console.log(queryString);
           refreshData = {this.processData}
           />
       }
-      
-      
         <section className={this.state.searchBlockClass} id="search-block">
         <div className="row-flex ayah-input-wrapper" >
           <div className="row-flex">
-            
             <SurahPopup />
-            <div className="triangle-right"><div className="inner"></div></div>
+            
             {this.state.selSurahNumber > 0 && 
               <p ref={(sur) => { this.surahLabel = sur; }} className="surah-name">{this.state.selSurahNumber}:</p>
             }
@@ -656,8 +477,6 @@ console.log(queryString);
           <a href="javascript:;" onClick={this.searchForAyah} role="button" className="search-btn">Search</a>
           
          </div> 
-        
-          
         </section>
       <div className='content-wrapper'>
         {listview}
@@ -668,12 +487,9 @@ console.log(queryString);
         <Loader />
         
       }
-     
-           
     </div>
-    
   );
   }
 }
-//onChange={handleChange('checkedF')}
+
 export default App;
